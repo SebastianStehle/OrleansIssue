b508776a3b9edbb27f5a68f3e896767d6cfeb830
Sebastian <sebastian@squidex.io>
2022-02-22 20:26:09 +0100

    Initial.

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Orleans;
+using Orleans.Hosting;
+
+class Filter : IIncomingGrainCallFilter
+{
+    public Task Invoke(IIncomingGrainCallContext context)
+    {
+        return Task.CompletedTask;
+    }
+}
+
+public interface IMyGrain : IGrainWithGuidKey
+{
+    Task Foo();
+}
+
+public class MyGrain : Grain, IMyGrain
+{
+    public Task Foo()
+    {
+        return Task.CompletedTask;
+    }
+}
+
+public class HostedService : IHostedService
+{
+    private readonly IGrainFactory factory;
+
+    public HostedService(IGrainFactory factory)
+    {
+        this.factory = factory;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        var grain = factory.GetGrain<IMyGrain>(Guid.NewGuid());
+
+        return grain.Foo();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public static class Program
+{
+    public static void Main()
+    {
+        Host.CreateDefaultBuilder()
+            .UseOrleans(builder =>
+            {
+                builder.UseLocalhostClustering();
+                builder.AddIncomingGrainCallFilter<Filter>();
+            })
+            .ConfigureServices(services =>
+            {
+                services.AddSingleton<IHostedService, HostedService>();
+            })
+            .Build()
+            .Run();
+    }
+}
